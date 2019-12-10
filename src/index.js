38c340935216a5f5343d056da878c6cedab51c02
ToniMihajlovski <toni@sopro.io>
2019-12-10 21:31:54 +0100

    added Redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import './index.css';
@@ -25,7 +25,8 @@ const logger = store => {
   }
 }
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
